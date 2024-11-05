e7466cb6587b94aa73b15a8f8edb8c463d416956
Tanishq Varshney <tanishqanivarshney@gmail.com>
2024-11-05 07:56:31 +0530

    Add APIError

---
+++ src/utils/apiError.js
@@ -0,0 +1,24 @@
+class APIError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong.", error = [],
+        stack = ""){
+            super(message);
+            this.statusCode = statusCode;
+            this.data = null;
+            this.message = message;
+            this.success = False;
+            this.errors = error;
+
+            if (stack)
+            {
+                this.stack = stack;
+            }
+            else
+            {
+                Error.captureStackTrace(this, this.constructor);
+            }
+        }
+    }
+
+export {APIError}
